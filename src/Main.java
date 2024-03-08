2b2a630f03d9cd5c8c8b42426b6f6ac61e1c5152
chris <drikosx323@gmail.com>
2024-03-08 18:01:10 +0200

    Added addTwoNumbers method, Leetcode problem

--- src/Main.java
+++ src/Main.java
@@ -264,6 +264,26 @@ public class Main {
         return dummy.next;
     }
 
+    public ListNode addTwoNumbers(ListNode a, ListNode b){
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        int carry =0;
+        while(a!= null || b!= null){
+            int x = (a!=null) ? a.data : 0;
+            int y = (b!= null) ? b.data : 0;
+            int sum = x+y+carry;
+            carry = sum/10;
+            tail.next = new ListNode(sum%10);
+            tail = tail.next;
+            if (a!=null) a = a.next;
+            if (b!= null) b = b.next;
+        }
+        if (carry>0){
+            tail.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
+
 
     public static void main(String[] args) {
         Main sll = new Main();
